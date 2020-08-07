be089060f8e2385945fbe682de548549ff035a26
zhengshaohong <zhengshaohong123>
2020-08-07 20:16:38 +0800

    rest code

--- spring-boot/src/main/java/cn/edu/cqive/WebApplication.java
+++ spring-boot/src/main/java/cn/edu/cqive/WebApplication.java
@@ -9,8 +9,6 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan
 public class WebApplication {
 
-
-
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
